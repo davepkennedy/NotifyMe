4ed916bd53d979be9bcf9cc5065fea5859617ad6
Dave Kennedy <davepkennedy@gmail.com>
2015-05-21 20:22:53 +0100

    Initial Commit

---
+++ NotifyMe/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NotifyMe
+//
+//  Created by Dave Kennedy on 21/05/2015.
+//  Copyright (c) 2015 Dave Kennedy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ NotifyMe/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  NotifyMe
+//
+//  Created by Dave Kennedy on 21/05/2015.
+//  Copyright (c) 2015 Dave Kennedy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
